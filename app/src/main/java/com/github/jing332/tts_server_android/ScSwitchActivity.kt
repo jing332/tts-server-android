50a3c8e27ad326a487f418e317de87f7537047b1
jing332 <3033784236@qq.com>
2022-09-28 19:30:25 +0800

    使用Theme隐藏ScSwitchActivity

--- app/src/main/java/com/github/jing332/tts_server_android/ScSwitchActivity.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/ScSwitchActivity.kt
@@ -1,15 +1,13 @@
 package com.github.jing332.tts_server_android
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
-class ScSwitchActivity : AppCompatActivity() {
+class ScSwitchActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /* 隐藏Activity */
-        moveTaskToBack(true)
         setContentView(R.layout.activity_none)
 
         if (TtsIntentService.IsRunning) {
@@ -17,6 +15,7 @@ class ScSwitchActivity : AppCompatActivity() {
             Toast.makeText(this, "服务已关闭", Toast.LENGTH_SHORT).show()
         } else {
             val i = Intent(this.applicationContext, TtsIntentService::class.java)
+            i.putExtra("isWakeLock", SharedPrefsUtils.getWakeLock(this))
             startService(i)
             Toast.makeText(this, "服务已启动", Toast.LENGTH_SHORT).show()
         }
