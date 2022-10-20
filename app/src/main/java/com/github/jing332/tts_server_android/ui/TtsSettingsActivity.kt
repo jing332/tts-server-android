68daf0ced9375902b638494e6b91b036743db01a
Jing <42014615+jing332@users.noreply.github.com>
2022-10-20 18:53:58 +0800

    优化TTS配置界面

--- app/src/main/java/com/github/jing332/tts_server_android/ui/TtsSettingsActivity.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/ui/TtsSettingsActivity.kt
@@ -2,6 +2,10 @@ package com.github.jing332.tts_server_android.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.github.jing332.tts_server_android.R
 import com.github.jing332.tts_server_android.databinding.ActivityTtsSettingsBinding
 import com.github.jing332.tts_server_android.ui.fragment.TtsConfigFragment
@@ -12,27 +16,45 @@ class TtsSettingsActivity : AppCompatActivity() {
         const val TAG = "TtsSettingsActivity"
     }
 
-    private val binding: ActivityTtsSettingsBinding by lazy { ActivityTtsSettingsBinding.inflate(layoutInflater) }
+    private val binding: ActivityTtsSettingsBinding by lazy {
+        ActivityTtsSettingsBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val configFragment = TtsConfigFragment()
-        val logFragment = TtsLogFragment()
+        binding.viewPager.adapter = FragmentAdapter(this)
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
 
-        binding.bottomVNavigationView.setOnItemSelectedListener {
+        binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.navbar_config -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, configFragment)
-                        .commitNow()
-                }
-                R.id.navbar_log -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, logFragment).commitNow()
-                }
+                R.id.navbar_config -> binding.viewPager.setCurrentItem(0, true)
+                R.id.navbar_log -> binding.viewPager.setCurrentItem(1, true)
             }
             true
+        }
+    }
+
+    class FragmentAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
+        private val configFragment = TtsConfigFragment()
+        private val logFragment = TtsLogFragment()
+        private val fragmentList = arrayListOf(configFragment, logFragment)
+
+        override fun getItemCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentList[position]
         }
     }
 }
--- app/src/main/java/com/github/jing332/tts_server_android/ui/fragment/TtsLogFragment.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/ui/fragment/TtsLogFragment.kt
@@ -11,7 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.github.jing332.tts_server_android.LogLevel
 import com.github.jing332.tts_server_android.MyLog
 import com.github.jing332.tts_server_android.databinding.FragmentTtsLogBinding
 import com.github.jing332.tts_server_android.service.tts.SystemTtsService.Companion.ACTION_ON_LOG
@@ -23,15 +23,22 @@ class TtsLogFragment : Fragment() {
         const val TAG = "TtsLogFragment"
     }
 
-    private val logAdapter: LogViewAdapter by lazy { LogViewAdapter(arrayListOf()) }
-
-    private val mReceiver: MyReceiver by lazy { MyReceiver() }
-
     private val binding: FragmentTtsLogBinding by lazy {
         FragmentTtsLogBinding.inflate(
             layoutInflater
         )
     }
+    private val logAdapter: LogViewAdapter by lazy {
+        LogViewAdapter(
+            arrayListOf(
+                MyLog(
+                    LogLevel.INFO,
+                    "请保持此页面存活以监听实时日志"
+                )
+            )
+        )
+    }
+    private val mReceiver: MyReceiver by lazy { MyReceiver() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,29 +55,6 @@ class TtsLogFragment : Fragment() {
         layoutManager.stackFromEnd = true
         binding.recyclerViewLog.layoutManager = layoutManager
         binding.recyclerViewLog.adapter = logAdapter
-
-        /* 用来判断是否在日志列表最底部 以确认是否自动滚动 */
-        binding.recyclerViewLog.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-//                val layout = recyclerView.layoutManager as LinearLayoutManager
-//                if (layout.findLastVisibleItemPosition() == layout.itemCount - 1) {
-//                    Log.e(TAG, "在最底部")
-////                    Toast.makeText(this@TtsLogFragment.context, "在最底部", Toast.LENGTH_SHORT).show()
-//                    // We have reached the end of the recycler view.
-//                }
-//
-//                super.onScrolled(recyclerView, dx, dy)
-//            }
-//            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-//                mLastItemCount = recyclerView.layoutManager!!.itemCount
-//                /* 当前状态为停止滑动状态SCROLL_STATE_IDLE时 */
-//                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-//                    if (recyclerView.layoutManager is LinearLayoutManager) {
-//                        mLastPosition = layoutManager.findLastVisibleItemPosition()
-//                    }
-//                }
-//            }
-        })
 
         requireContext().registerReceiver(mReceiver, IntentFilter(ACTION_ON_LOG))
     }
@@ -96,9 +80,4 @@ class TtsLogFragment : Fragment() {
         }
 
     }
-
-    fun RecyclerView?.getCurrentPosition(): Int {
-        return (this?.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
-    }
-
 }
