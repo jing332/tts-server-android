702a62cfcbe73b4a2bd74b19bd550d1db26fae7a
Jing <42014615+jing332@users.noreply.github.com>
2023-02-09 20:09:06 +0800

    feat: 插入拼音声调界面的无障碍优化

---
+++ app/src/main/java/com/github/jing332/tts_server_android/util/SizeUtils.kt
@@ -0,0 +1,50 @@
+package com.github.jing332.tts_server_android.util
+
+import android.content.res.Resources
+
+object SizeUtils {
+
+    /**
+     * Value of dp to value of px.
+     *
+     * @param dpValue The value of dp.
+     * @return value of px
+     */
+    fun dp2px(dpValue: Float): Int {
+        val scale: Float = Resources.getSystem().displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of px to value of dp.
+     *
+     * @param pxValue The value of px.
+     * @return value of dp
+     */
+    fun px2dp(pxValue: Float): Int {
+        val scale: Float = Resources.getSystem().displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of sp to value of px.
+     *
+     * @param spValue The value of sp.
+     * @return value of px
+     */
+    fun sp2px(spValue: Float): Int {
+        val fontScale: Float = Resources.getSystem().displayMetrics.scaledDensity
+        return (spValue * fontScale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of px to value of sp.
+     *
+     * @param pxValue The value of px.
+     * @return value of sp
+     */
+    fun px2sp(pxValue: Float): Int {
+        val fontScale: Float = Resources.getSystem().getDisplayMetrics().scaledDensity
+        return (pxValue / fontScale + 0.5f).toInt()
+    }
+}
