8e6587b0585406477d2a067fb5d2118a4319e4c5
Jing <42014615+jing332@users.noreply.github.com>
2023-04-14 16:09:55 +0800

    chore: actions新增debug

--- app/src/main/java/com/github/jing332/tts_server_android/help/audio/InputStreamMediaDataSource.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/help/audio/InputStreamMediaDataSource.kt
@@ -2,6 +2,7 @@ package com.github.jing332.tts_server_android.help.audio
 
 import android.media.MediaDataSource
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import okio.buffer
 import okio.source
@@ -9,15 +10,33 @@ import java.io.InputStream
 
 @RequiresApi(Build.VERSION_CODES.M)
 class InputStreamMediaDataSource(private val inputStream: InputStream) : MediaDataSource() {
+    companion object {
+        const val TAG = "InputStreamDataSource"
+    }
+
     private val bufferedInputStream = inputStream.source().buffer()
 
     override fun close() {
+        Log.d(TAG, "close")
         bufferedInputStream.close()
     }
 
     override fun readAt(position: Long, buffer: ByteArray, offset: Int, size: Int): Int {
-        return bufferedInputStream.read(buffer, offset, size)
+        Log.d(TAG, "readAt: pos=$position, offset=$offset, size=$size")
+        kotlin.runCatching {
+            return bufferedInputStream.read(buffer, offset, size).apply {
+                Log.d(TAG, "readAt: readLen=$this")
+            }
+        }.onFailure {
+            Log.d(TAG, it.stackTraceToString())
+        }
+        return -1
     }
 
-    override fun getSize(): Long = inputStream.available().toLong()
+    override fun getSize(): Long {
+        return inputStream.available().toLong().apply {
+            Log.d(TAG, "getSize: $this")
+        }
+    }
+
 }
