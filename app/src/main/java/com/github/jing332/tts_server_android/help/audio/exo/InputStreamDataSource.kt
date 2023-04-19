06df454a04ea9251d9c1d679f2329aa20526ff50
Jing <42014615+jing332@users.noreply.github.com>
2023-04-19 14:19:00 +0800

    perf: ExoPlayer流

--- app/src/main/java/com/github/jing332/tts_server_android/help/audio/exo/InputStreamDataSource.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/help/audio/exo/InputStreamDataSource.kt
@@ -4,14 +4,18 @@ import android.net.Uri
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.upstream.BaseDataSource
 import com.google.android.exoplayer2.upstream.DataSpec
+import okio.buffer
+import okio.source
 import java.io.EOFException
 import java.io.IOException
 import java.io.InputStream
+import kotlin.math.min
 
 
 class InputStreamDataSource(
     private val inputStream: InputStream,
 ) : BaseDataSource(/* isNetwork = */ false) {
+    private val bufferedSource = inputStream.source().buffer()
     private var dataSpec: DataSpec? = null
     private var bytesRemaining: Long = 0
     private var opened = false
@@ -19,18 +23,15 @@ class InputStreamDataSource(
     @Throws(IOException::class)
     override fun open(dataSpec: DataSpec): Long {
         this.dataSpec = dataSpec
-        try {
-            val skipped: Long = inputStream.skip(dataSpec.position)
-            if (skipped < dataSpec.position) throw EOFException()
-            if (dataSpec.length != C.LENGTH_UNSET.toLong()) {
-                bytesRemaining = dataSpec.length
-            } else {
-                bytesRemaining = inputStream.available().toLong()
-                if (bytesRemaining == 0L) bytesRemaining = C.LENGTH_UNSET.toLong()
-            }
-        } catch (e: IOException) {
-            throw IOException(e)
+        bufferedSource.skip(dataSpec.position)
+
+        if (dataSpec.length == C.LENGTH_UNSET.toLong()) {
+            bytesRemaining = inputStream.available().toLong()
+            if (bytesRemaining == 0L) bytesRemaining = C.LENGTH_UNSET.toLong()
+        } else {
+            bytesRemaining = dataSpec.length
         }
+
         opened = true
         return bytesRemaining
     }
@@ -44,16 +45,12 @@ class InputStreamDataSource(
         } else if (bytesRemaining == 0L) {
             return C.RESULT_END_OF_INPUT
         }
-        val bytesRead: Int = try {
-            val bytesToRead =
-                if (bytesRemaining == C.LENGTH_UNSET.toLong()) readLength else Math.min(
-                    bytesRemaining,
-                    readLength.toLong()
-                ).toInt()
-            inputStream.read(buffer, offset, bytesToRead)
-        } catch (e: IOException) {
-            throw IOException(e)
-        }
+
+        val bytesToRead =
+            if (bytesRemaining == C.LENGTH_UNSET.toLong()) readLength
+            else min(bytesRemaining, readLength.toLong()).toInt()
+
+        val bytesRead = bufferedSource.read(buffer, offset, bytesToRead)
         if (bytesRead == -1) {
             if (bytesRemaining != C.LENGTH_UNSET.toLong()) {
                 // End of stream reached having not read sufficient data.
@@ -61,22 +58,21 @@ class InputStreamDataSource(
             }
             return C.RESULT_END_OF_INPUT
         }
-        if (bytesRemaining != C.LENGTH_UNSET.toLong()) {
+        if (bytesRemaining != C.LENGTH_UNSET.toLong())
             bytesRemaining -= bytesRead.toLong()
-        }
+
         return bytesRead
     }
 
     @Throws(IOException::class)
     override fun close() {
         try {
+            bufferedSource.close()
             inputStream.close()
         } catch (e: IOException) {
             throw e
         } finally {
-            if (opened) {
-                opened = false
-            }
+            opened = false
         }
     }
 }
