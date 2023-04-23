60e7bcd2cd595fa241e0cb22702acfdf7f71ac8d
Jing <42014615+jing332@users.noreply.github.com>
2023-04-23 10:04:36 +0800

    refactor: 朗读规则的调试移到IO线程

--- app/src/main/java/com/github/jing332/tts_server_android/ui/systts/speech_rule/SpeechRuleEditorViewModel.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/ui/systts/speech_rule/SpeechRuleEditorViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.github.jing332.tts_server_android.constant.SpeechTarget
 import com.github.jing332.tts_server_android.data.appDb
 import com.github.jing332.tts_server_android.data.entities.SpeechRule
@@ -12,6 +13,8 @@ import com.github.jing332.tts_server_android.model.rhino.ExceptionExt.lineMessag
 import com.github.jing332.tts_server_android.model.rhino.core.Logger
 import com.github.jing332.tts_server_android.model.rhino.speech_rule.SpeechRuleEngine
 import com.script.ScriptException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SpeechRuleEditorViewModel(application: Application) : AndroidViewModel(application) {
     private val _errorLiveData: MutableLiveData<Throwable> = MutableLiveData()
@@ -64,32 +67,34 @@ class SpeechRuleEditorViewModel(application: Application) : AndroidViewModel(app
     }
 
     fun debug(text: String) {
-        if (evalRuleInfo()) {
-            kotlin.runCatching {
-                logger.i("handleText()...")
+        viewModelScope.launch(Dispatchers.IO) {
+            if (evalRuleInfo()) {
+                kotlin.runCatching {
+                    logger.i("handleText()...")
 
-                val rules = appDb.systemTtsDao.getEnabledList(SpeechTarget.CUSTOM_TAG).map {
-                    it.speechRule.apply { configId = it.id }
-                }
-                val list = mRuleEngine.handleText(text, rules)
-                try {
-                    list.forEach {
-                        val texts = mRuleEngine.splitText(it.text)
-                        logger.i(
-                            "\ntag=${it.tag}, id=${it.id}, text=${it.text.trim()}, splittedTexts=${
-                                texts.joinToString(" | ").trim()
-                            }"
-                        )
+                    val rules = appDb.systemTtsDao.getEnabledList(SpeechTarget.CUSTOM_TAG).map {
+                        it.speechRule.apply { configId = it.id }
                     }
-                } catch (_: NoSuchMethodException) {
-                }
-            }.onFailure {
-                if (it is ScriptException) {
-                    logger.e(it.lineMessage())
-                } else {
-                    logger.e(it.stackTraceToString())
-                }
+                    val list = mRuleEngine.handleText(text, rules)
+                    try {
+                        list.forEach {
+                            val texts = mRuleEngine.splitText(it.text)
+                            logger.i(
+                                "\ntag=${it.tag}, id=${it.id}, text=${it.text.trim()}, splittedTexts=${
+                                    texts.joinToString(" | ").trim()
+                                }"
+                            )
+                        }
+                    } catch (_: NoSuchMethodException) {
+                    }
+                }.onFailure {
+                    if (it is ScriptException) {
+                        logger.e(it.lineMessage())
+                    } else {
+                        logger.e(it.stackTraceToString())
+                    }
 
+                }
             }
         }
     }
