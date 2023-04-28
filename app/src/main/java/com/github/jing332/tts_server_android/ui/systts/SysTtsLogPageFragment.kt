824efa929e53c3b92111dfe2e81e5ee882901edd
Jing <42014615+jing332@users.noreply.github.com>
2023-04-28 21:46:00 +0800

    feat: 日志界面

--- app/src/main/java/com/github/jing332/tts_server_android/ui/systts/SysTtsLogPageFragment.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/ui/systts/SysTtsLogPageFragment.kt
@@ -5,8 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.jing332.tts_server_android.R
 import com.github.jing332.tts_server_android.constant.AppConst
@@ -17,18 +22,19 @@ import com.github.jing332.tts_server_android.ui.AppLog
 import com.github.jing332.tts_server_android.ui.view.BrvLogHelper
 import com.github.jing332.tts_server_android.ui.view.adapter.LogListItemAdapter
 
-class SysTtsLogPageFragment : Fragment(R.layout.systts_log_fragment) {
+class SysTtsLogPageFragment : Fragment(R.layout.systts_log_fragment), MenuProvider {
     companion object {
         const val TAG = "TtsLogFragment"
     }
 
     private val binding by viewBinding(SysttsLogFragmentBinding::bind)
-    private val logAdapter: LogListItemAdapter by lazy { LogListItemAdapter(isHtmlText = true) }
     private val mReceiver: MyReceiver by lazy { MyReceiver() }
     private var mLogHelper: BrvLogHelper? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         mLogHelper = BrvLogHelper(binding.recyclerViewLog, isHtmlEnabled = true)
         mLogHelper?.bindFloatingActionButton(binding.btnToBottom)
@@ -51,5 +57,20 @@ class SysTtsLogPageFragment : Fragment(R.layout.systts_log_fragment) {
                 mLogHelper?.append(log)
             }
         }
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.log, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.menu_clear_log -> {
+                mLogHelper?.removeAll()
+
+            }
+        }
+
+        return true
     }
 }
