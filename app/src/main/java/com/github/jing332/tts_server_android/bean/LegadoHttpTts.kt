70fd47aa812ea233f1bcb956eadfc13bc8f5610c
Jing <42014615+jing332@users.noreply.github.com>
2023-06-29 21:29:28 +0800

    fix: 导入阅读TTS失败

--- app/src/main/java/com/github/jing332/tts_server_android/bean/LegadoHttpTts.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/bean/LegadoHttpTts.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class LegadoHttpTts(
-    @SerialName("header") val header: String,
-    @SerialName("id") val id: Long,
-    @SerialName("name") val name: String,
-    @SerialName("url") val url: String
+    @SerialName("header") val header: String = "",
+    @SerialName("id") val id: Long = 0,
+    @SerialName("name") val name: String = "",
+    @SerialName("url") val url: String = ""
 
 //    @SerialName("concurrentRate")
 //    val concurrentRate: String,
