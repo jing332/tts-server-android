d392128d171c3bdf52a4d88c5712bc291ae7e231
Jing <42014615+jing332@users.noreply.github.com>
2023-07-25 19:52:22 +0800

    refactor: 优化导入配置的格式识别

--- app/src/main/java/com/github/jing332/tts_server_android/help/ConfigImportHelper.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/help/ConfigImportHelper.kt
@@ -4,22 +4,36 @@ import com.github.jing332.tts_server_android.constant.ConfigType
 
 object ConfigImportHelper {
     private val fieldMap = mapOf(
-        "pluginId" to ConfigType.PLUGIN,
+        listOf("pluginId", "code", "version") to ConfigType.PLUGIN,
         "pattern" to ConfigType.REPLACE_RULE,
-        "ruleId" to ConfigType.SPEECH_RULE,
-        "list" to ConfigType.LIST,
+        listOf("ruleId", "tags") to ConfigType.SPEECH_RULE,
+        listOf("list", "tts", "displayName", "#type") to ConfigType.LIST,
     )
 
     /**
      * 检查配置字符串是否符合配置格式
      */
     fun getConfigType(str: String): ConfigType {
-        fieldMap.forEach {
-            if (str.contains("\"${it.key}\":")) {
-                return it.value
+        fieldMap.forEach { entry ->
+            when (entry.key) {
+                is String -> if (str.contains("\"${entry.key}\":")) {
+                    return entry.value
+                }
+
+                is List<*> -> if (str.contains((entry.key as List<*>).map { "\"${it.toString()}\":" })) {
+                    return entry.value
+                }
             }
         }
 
         return ConfigType.UNKNOWN
+    }
+
+    private fun String.contains(list: List<String>): Boolean {
+        var count = 0
+        list.forEach {
+            if (this.contains(it)) count++
+        }
+        return count == list.size
     }
 }
